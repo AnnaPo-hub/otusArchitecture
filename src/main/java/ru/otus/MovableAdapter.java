ead3446f2f5c902230af5642d06b2525b0996055
Anna Popova <anna.popova201909@gmail.com>
2026-02-04 14:02:49 +0300

    add Imovable interface and Move class

---
+++ src/main/java/ru/otus/MovableAdapter.java
@@ -0,0 +1,19 @@
+package ru.otus;
+
+public class MovableAdapter implements IMovable{
+
+    @Override
+    public Vector getLocation() {
+        return null;
+    }
+
+    @Override
+    public void setLocation(Vector newValue) {
+
+    }
+
+    @Override
+    public Vector getVelocity() {
+        return null;
+    }
+}

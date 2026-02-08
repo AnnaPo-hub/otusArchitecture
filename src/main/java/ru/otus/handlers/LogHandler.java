6b91228510266d70bc56720329b01f670e4dc596
Anna Popova <anna.popova201909@gmail.com>
2026-02-08 14:38:21 +0300

    added Tests

--- src/main/java/ru/otus/handlers/LogHandler.java
+++ src/main/java/ru/otus/handlers/LogHandler.java
@@ -10,10 +10,10 @@ import java.util.Queue;
 public class LogHandler implements IExceptionHandler {
     @Override
     public void handle(ICommand cmd, Exception exc, Queue<ICommand> queue) {
-        if (cmd instanceof RetryOnceCommand) {
-            queue.add(new LogExceptionCommand(((RetryOnceCommand) cmd).getOriginalCommand(), exc));
-        } else if (cmd instanceof RetryTwiceCommand) {
+        if (cmd instanceof RetryTwiceCommand) {
             queue.add(new LogExceptionCommand(((RetryTwiceCommand) cmd).getOriginalCommand(), exc));
+        } else if (cmd instanceof RetryOnceCommand) {
+            queue.add(new LogExceptionCommand(((RetryOnceCommand) cmd).getOriginalCommand(), exc));
         } else {
             queue.add(new LogExceptionCommand(cmd, exc));
         }
